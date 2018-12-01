857f3fa7e79f1abb44015da3f60b2541f6a9f965
Steven F <WestFR@users.noreply.github.com>
2018-12-01 16:05:01 +0100

    Add dev dependencies and nelmio doc
    - Add 'composer require server --dev' (for have console server commands)
    - Add 'composer req cors' (for cors allow communications)
    - Add 'composer require nelmio/api-doc-bundle' (for api doc)
    
    - Expose assets and swagger-ui

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,8 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
